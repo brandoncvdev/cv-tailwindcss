c6b0b3722c4967e9368211451383d167bf806c3d
Brandon <bcastillo@dannyyesoft.mx>
2021-12-18 22:09:33 -0600

    nothing change

--- src/app/pages/about-me/about-me.module.ts
+++ src/app/pages/about-me/about-me.module.ts
@@ -4,15 +4,9 @@ import { AboutMeRoutingModule } from './about-me-routing.module';
 
 import { AboutMeComponent } from './about-me.component';
 import { InfoSkillModule } from '@shared/components/info-skill/info-skill.module';
-import { StepperModule } from '@shared/components/stepper/stepper.module';
 
 @NgModule({
     declarations: [AboutMeComponent],
-    imports: [
-        CommonModule,
-        AboutMeRoutingModule,
-        InfoSkillModule,
-        StepperModule,
-    ],
+    imports: [CommonModule, AboutMeRoutingModule, InfoSkillModule],
 })
 export class AboutMeModule {}
